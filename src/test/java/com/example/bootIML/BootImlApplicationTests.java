b9d16623ac8ef98637cb8135aaabb660e39f9217
kharo1963 <kharo1963@gmail.com>
2023-09-17 18:19:46 +0300

    interface refinement 3

--- src/test/java/com/example/bootIML/BootImlApplicationTests.java
+++ src/test/java/com/example/bootIML/BootImlApplicationTests.java
@@ -1,38 +1,49 @@
 package com.example.bootIML;
 
-import com.example.bootIML.interpretator.Interpretator;
-import com.example.bootIML.interpretator.StatD;
-import com.example.bootIML.service.ArrayFilFiles;
+import com.example.bootIML.interpretator.SourceProgram;
+import com.example.bootIML.service.InterpretatorService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+@Slf4j
 @SpringBootTest
 class BootImlApplicationTests {
 
+	@Autowired
+	private InterpretatorService interpretatorService;
 	@Test
 	void contextLoads() {
-		System.out.println("TEST: contexLoads");
+		log.info("TEST: contexLoads");
 	}
 
 	@Test
 	void testInterpretator() {
-		String srcCode = System.getProperty("user.dir") + System.getProperty("file.separator") + "ext-gcd.txt";
-		System.out.println("TEST: testInterpretator");
-
-		StatD.TID = new ArrayList<>();
-		StatD.restArg = new ArrayList<>();
-		ArrayFilFiles.filFiles = new ArrayList();
-
-		System.out.println(srcCode);
-		Interpretator I = new Interpretator(srcCode);
-		I.interpretation();
-
-		ArrayFilFiles.filFiles.forEach(s -> System.out.println(s));
+  	String srcCode = System.getProperty("user.dir") + System.getProperty("file.separator") + "m-ext-gcd.txt";
+		log.info("TEST: testInterpretator");
+		log.info(srcCode);
+		String sourceText = "";
+		Path path = Paths.get(srcCode);
+		try {
+			List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+			for (String line : lines) {
+				sourceText += line + System.lineSeparator();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		SourceProgram sourceProgram = interpretatorService.invokeInterpretator(sourceText);
+		sourceProgram.resultList.forEach(s -> System.out.println(s));
 		List<Integer> tstFiles = List.of(5, 3, 1, -1, 2);
-		Assertions.assertArrayEquals(ArrayFilFiles.filFiles.toArray(), tstFiles.toArray());
+		Assertions.assertArrayEquals(sourceProgram.resultList.toArray(), tstFiles.toArray());
 	}
 }
