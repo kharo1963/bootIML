441ca67a7934b9713ff550b0d14537b8503ae05d
kharo1963 <kharo1963@gmail.com>
2023-11-07 23:38:07 +0300

    yandex cloud

--- src/main/java/com/example/bootIML/BootImlApplication.java
+++ src/main/java/com/example/bootIML/BootImlApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootImlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootImlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BootImlApplication.class, args);
+    }
 
 }
