79a797fb3854e40f22d2c779fac99999519d9468
kharo1963 <kharo1963@gmail.com>
2023-11-07 23:26:52 +0300

    yandex cloud

--- src/main/java/com/example/bootIML/model/ImlParam.java
+++ src/main/java/com/example/bootIML/model/ImlParam.java
@@ -1,13 +1,21 @@
 package com.example.bootIML.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "iml_param")
 public class ImlParam {
+
     @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Integer id;
 
     @Column(name = "prog_name")
@@ -18,36 +26,5 @@ public class ImlParam {
 
     @Column(name = "param_val")
     private Integer paramVal;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getProgName() {
-        return progName;
-    }
-
-    public void setProgName(String progName) {
-        this.progName = progName;
-    }
-
-    public String getParamName() {
-        return paramName;
-    }
-
-    public void setParamName(String paramName) {
-        this.paramName = paramName;
-    }
-
-    public Integer getParamVal() {
-        return paramVal;
-    }
-
-    public void setParamVal(Integer paramVal) {
-        this.paramVal = paramVal;
-    }
 }
+
