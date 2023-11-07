4b32ac7addcb9ba80a82d63c6ea1859dbaa87069
kharo1963 <kharo1963@gmail.com>
2023-11-07 23:41:33 +0300

    yandex cloud

---
+++ src/main/java/com/example/bootIML/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.example.bootIML.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification ",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        }
+)
+public class OpenApiConfig {
+}
